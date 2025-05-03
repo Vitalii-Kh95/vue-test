7229588f988c55a1b097454ef2fb4864182a07e1
Vitalii-Kh95 <mew030995@gmail.com>
2025-05-04 01:20:39 +0500

    resumeblock's style changes on window size change

--- src/components/resume blocks/ResumeBlock.vue
+++ src/components/resume blocks/ResumeBlock.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="relative m-1 flex w-full flex-col text-base-content">
-    <GradientHeader :title="title" />
-    <div class="ml-[1px] p-4">
+    <RibbonHeader v-if="breakpoints.active().value !== 'md'" :title="title" />
+    <GradientHeader v-else :title="title" />
+    <div
+      class="ml-[1px] p-4"
+      :class="breakpoints.active().value !== 'md' ? 'mt-11 rounded-b-lg bg-base-100 shadow-md' : ''"
+    >
       <div class="w-full">
         <slot />
       </div>
@@ -11,6 +15,10 @@
 
 <script setup>
 import GradientHeader from './GradientHeader.vue';
+import RibbonHeader from './RibbonHeader.vue';
+import { useBreakpoints, breakpointsTailwind } from '@vueuse/core';
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
 defineProps({
   title: String
 });
