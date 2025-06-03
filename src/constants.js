117358c3dffba3f03d826271ce52469631ee3336
Vitalii-Kh95 <mew030995@gmail.com>
2025-06-03 11:06:37 +0500

    small changes, dev related

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 // export const baseURL = 'http://localhost:8000/api/';
 
 // dev level environment variable. Defined in vite.config file
-// eslint-disable-next-line
-export const baseURL = `http://${__HOST_IP__}:8000/api/`;
 
+// export const baseURL = `http://${__HOST_IP__}:8000/api/`;
+export const baseURL = 'https://myapp.test/api/';
 // To not bother with path resolution I put image to public dir instead of src/assets
 export const projects = [
   {
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,54 +1,39 @@
 import { fileURLToPath, URL } from 'node:url';
-import os from 'os';
+// import os from 'os';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import fs from 'fs';
+import path from 'path';
 
-function getLocalIPAddress() {
-  const interfaces = os.networkInterfaces();
-  for (const iface of Object.values(interfaces)) {
-    for (const alias of iface) {
-      if (alias.family === 'IPv4' && !alias.internal) {
-        return alias.address;
-      }
-    }
-  }
-  return 'localhost';
-}
+// function getLocalIPAddress() {
+//   const interfaces = os.networkInterfaces();
+//   for (const iface of Object.values(interfaces)) {
+//     for (const alias of iface) {
+//       if (alias.family === 'IPv4' && !alias.internal) {
+//         return alias.address;
+//       }
+//     }
+//   }
+//   return 'localhost';
+// }
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    __HOST_IP__: JSON.stringify(getLocalIPAddress())
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost-key.pem')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.pem'))
+    },
+    host: 'localhost', // so you can open https://localhost:3000
+    port: 3000
   },
+  // define: {
+  //   __HOST_IP__: JSON.stringify(getLocalIPAddress())
+  // },
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-  // server: {
-  //   host: true,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8000',
-  //       changeOrigin: true,
-  //       secure: false
-  //     }
-  //   }
-  // }
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://127.0.0.1:8000', // Your Django backend
-  //       changeOrigin: true, // Needed if Django is on a different origin
-  //       secure: false // If using HTTPS locally, set to true
-  //     },
-  //     '/media': {
-  //       // Add this rule to proxy media files
-  //       target: 'http://127.0.0.1:8000',
-  //       changeOrigin: true,
-  //       secure: false
-  //     }
-  //   }
-  // }
 });
